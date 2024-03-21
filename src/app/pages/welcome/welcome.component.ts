697568555d9d7258b42031f58fed9337f8900a36
leon_lin <leon_lin@gss.com.tw>
2024-03-21 14:37:59 +0800

    add get books data

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { HttpserviceService } from '../../service/httpservice.service';
 
@@ -9,15 +10,29 @@ import { HttpserviceService } from '../../service/httpservice.service';
 })
 export class WelcomeComponent implements OnInit {
 
-  message: string="";
+  message: string = "";
+  books: any[] = [];
 
-  constructor(private httpService: HttpserviceService) {}
+  constructor(private httpService: HttpserviceService, private http: HttpClient) { }
 
   ngOnInit() {
   }
 
-  Send(){
+  Send() {
     this.httpService.sendLineNotifyApi(this.message)
   }
 
+  getBooks() {
+    this.http.get<any[]>('https://nodetestapi.azurewebsites.net/api/books')
+      .subscribe(
+        (response) => {
+          this.books = response;
+          console.log('Books:', this.books);
+        },
+        (error) => {
+          console.error('Error fetching books:', error);
+        }
+      );
+
+  }
 }
